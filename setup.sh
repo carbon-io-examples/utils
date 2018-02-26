a1b3de98aa009c1b43ab3bd532d9622ef16ea44f
Tim Fogarty <timothy.fogo@gmail.com>
2018-02-26 13:14:19 -0800

    Update to package-json-merge

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 curl https://api.github.com/orgs/carbon-io-examples/repos\?per_page\=200 | jq -r '.[].ssh_url' | xargs -L 1 git clone
-ls */package.json | xargs package-merge > package.json
+ls */package.json | xargs package-json-merge > package.json
 npm i && npm link @carbon-io/carbond
